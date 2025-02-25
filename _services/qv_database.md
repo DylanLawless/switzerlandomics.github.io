4438a274b6e9844549c1951ca8ba573470804855
DylanLawless <dylan.lawless@protonmail.com>
2025-02-25 13:35:20 +0100

    qv example db

---
+++ _services/qv_database.md
@@ -0,0 +1,17 @@
+---
+title: "Qualifying Variant database"
+date: 2025-01-01T00:00:01+10:00
+weight: 1
+---
+
+Enhancing precision in genomic interpretation through FAIR data, open and reproducible analysis criteria.
+
+The Qualifying Variant (QV) Database offers an open, shareable, and reproducible framework for genomic interpretation. Central to this framework are the QV criteria YAML files, which define specific genomic variant criteria used within analysis pipelines. These files provide structured, machine-readable definitions that standardise how variants are selected and interpreted across different genomic studies, ensuring consistency and reproducibility in genetic analysis. Demonstrated effectively in our validation case studies, these files facilitate streamlined and accurate variant interpretation, critical for both research and clinical diagnostics.
+FAIR data is data which meets the FAIR principles of findability, accessibility, interoperability, and reusability
+([Wilkinson et al 2016](https://doi.org/10.1038%2FSDATA.2016.18)).
+
+## QV YAML Files SHA256 Checksums
+
+| View Link | Direct Download | Filename | SHA256 |
+| --------- | --------------- | -------- | ------ |
+| [View](/assets/qv_database/qv_acmg_svnindel_criteria_20250225.yaml) | [Download](/assets/qv_database/qv_acmg_svnindel_criteria_20250225.yaml){:download="qv_acmg_svnindel_criteria_20250225.yaml"} | qv_acmg_svnindel_criteria_20250225.yaml | `d91fde41a5fff48631adecba38773d619ae8cd5cff9b9b42ef7f5efbd6bbfcdf` |
---
+++ _site/assets/qv_database/generate_qv_database.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Navigate to the correct directory
+cd ~/web/switzerlandomics.github.io/_services
+
+# Start by creating a new Markdown file with front matter
+cat > qv_database.md <<EOF
+---
+title: "Qualifying Variant database"
+date: 2025-01-01T00:00:01+10:00
+weight: 1
+---
+
+Enhancing precision in genomic interpretation through FAIR data, open and reproducible analysis criteria.
+
+The Qualifying Variant (QV) Database offers an open, shareable, and reproducible framework for genomic interpretation. Central to this framework are the QV criteria YAML files, which define specific genomic variant criteria used within analysis pipelines. These files provide structured, machine-readable definitions that standardise how variants are selected and interpreted across different genomic studies, ensuring consistency and reproducibility in genetic analysis. Demonstrated effectively in our validation case studies, these files facilitate streamlined and accurate variant interpretation, critical for both research and clinical diagnostics.
+FAIR data is data which meets the FAIR principles of findability, accessibility, interoperability, and reusability
+([Wilkinson et al 2016](https://doi.org/10.1038%2FSDATA.2016.18)).
+
+## QV YAML Files SHA256 Checksums
+
+| View Link | Direct Download | Filename | SHA256 |
+| --------- | --------------- | -------- | ------ |
+EOF
+
+# Iterate through each YAML file in the assets/qv_database directory
+for file in ../assets/qv_database/*.yaml; do
+    # Compute SHA256 checksum
+    checksum=$(shasum -a 256 "$file" | awk '{print $1}')
+    # Extract filename for hyperlink
+    filename=$(basename "$file")
+    # Format row in markdown table with view, download links, filename, and SHA256
+    echo "| [View](/assets/qv_database/$filename) | [Download](/assets/qv_database/$filename){:download=\"$filename\"} | ${filename} | \`${checksum}\` |" >> qv_database.md
+done
+
